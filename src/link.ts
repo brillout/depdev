90af6964fc7a043db13f7eac19b8f741ead413f7
Romuald Brillout <git@brillout.com>
2022-07-22 18:23:06 +0200

    improve link command

--- src/link.ts
+++ src/link.ts
@@ -3,33 +3,71 @@ export { link }
 import { loadPackageJson } from './loadPackageJson'
 import { runCommand } from './runCommand'
 import path from 'path'
+import fs from 'fs'
+import assert from 'assert'
 import { mkdirp } from './utils'
 
 async function link(pkgName: string) {
+  const workspaceRoot = findWorkspaceRoot()
+
+  mkdirp('deps', workspaceRoot)
+
   const { owner, repo } = getGitRepo(pkgName)
-  const depDirRelative = `./deps/${repo}/`
-  const depDirAbsolute = path.join(process.cwd(), depDirRelative)
+  const pkgDir = path.join(workspaceRoot, `./deps/${repo}/`)
 
-  mkdirp('deps')
-
-  // `-q` to avoid `git clone` to write progress messages to stderr, see https://stackoverflow.com/questions/32685568/git-clone-writes-to-sderr-fine-but-why-cant-i-redirect-to-stdout
-  await runCommand(`git clone git@github.com:${owner}/${repo} -q`, {
-    cwd: './deps/',
-    timeout: 15 * 1000,
-    print: 'overview'
-  })
+  if (!fs.existsSync(pkgDir)) {
+    // `-q` to avoid `git clone` to write progress messages to stderr, see https://stackoverflow.com/questions/32685568/git-clone-writes-to-sderr-fine-but-why-cant-i-redirect-to-stdout
+    await runCommand(`git clone git@github.com:${owner}/${repo} -q`, {
+      cwd: path.join(workspaceRoot, `./deps/`),
+      timeout: 15 * 1000,
+      print: 'overview'
+    })
+  } else {
+    const cwd = pkgDir
+    const stdout = await runCommand(`git status --porcelain`, { cwd })
+    assert(stdout !== null)
+    const isDirty = stdout !== ''
+    if (isDirty) {
+      console.log(`Uncommitted changes at ${pkgDir}`)
+    } else {
+      const print = 'overview'
+      await runCommand(`git fetch`, { cwd, print, timeout: 15 * 1000 })
+      await runCommand(`git merge`, { cwd, print })
+    }
+  }
+  assert(fs.existsSync(pkgDir))
 
   // `pnpm link` also runs `pnpm install`
-  await runCommand(`pnpm link ${depDirAbsolute}`, {
+  await runCommand(`pnpm link ${pkgDir}`, {
     timeout: 120 * 1000,
     print: 'overview'
   })
 
+  /*
   await runCommand('pnpm run dev', {
-    cwd: depDirRelative,
+    cwd: pkgDir,
     timeout: null,
     print: 'all'
   })
+  //*/
+}
+
+function findWorkspaceRoot(): string {
+  const dirCurrent = process.cwd()
+  let dir = dirCurrent
+  const filesystemRoot = getFilesystemRoot()
+  while (dir !== filesystemRoot) {
+    try {
+      return path.dirname(require.resolve(path.join(dir, 'pnpm-workspace.yaml')))
+    } catch {}
+    dir = path.dirname(dir)
+  }
+  return dirCurrent
+}
+
+function getFilesystemRoot() {
+  // https://stackoverflow.com/questions/9652043/identifying-the-file-system-root-with-node-js/50299531#50299531
+  return path.parse(process.cwd()).root
 }
 
 function getGitRepo(pkgName: string) {
--- src/utils/mkdirp.ts
+++ src/utils/mkdirp.ts
@@ -1,9 +1,11 @@
 export { mkdirp }
 
 import fs from 'fs'
+import path from 'path'
 
-async function mkdirp(dirName: string) {
-  if (!fs.existsSync(dirName)) {
-    fs.mkdirSync(dirName)
+async function mkdirp(dirName: string, cwd: string) {
+  const dirPath = path.join(cwd, dirName)
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath)
   }
 }
