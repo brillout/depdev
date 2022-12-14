bd2312f54796bef75f77e328c89e47405680aacb
Romuald Brillout <git@brillout.com>
2022-12-14 13:21:57 +0100

    debug

--- src/postinstall.ts
+++ src/postinstall.ts
@@ -4,6 +4,9 @@ import { getConfig, loadConfig } from './config'
 import { link } from './link'
 
 async function postinstall() {
+  console.log('postinstall()')
+  console.log('process.cwd()', process.cwd())
+  console.log('process.env.INIT_CWD', process.env.INIT_CWD)
   await loadConfig()
   const config = getConfig()
   const { inlinedDependencies } = config
